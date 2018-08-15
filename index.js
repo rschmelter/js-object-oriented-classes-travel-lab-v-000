4645739de424316d2bdcf7e2fac45b81a5a9f786
Ryan Schmelter <rschmelter33@gmail.com>
2018-08-15 23:41:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ class Driver {
     this.startDate = new Date(startDate);
   }
 
+  yearsExperienceFromBeginningOf(year) {
+    let endDate = new Date(year, 1, 1)
+    let totalYears = (endDate - this.startDate)/(365*24*60*60*1000)
+    return parseInt(totalYears)
+
+  }
+
 
 }
 
